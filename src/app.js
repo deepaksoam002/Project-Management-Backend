dfd933c79b0e1abc986dbfa00bd8d42ba994a558
deepaksoam002 <deepaksoam319@gmail.com>
2026-08-05 12:54:13 +0530

    Added healthcheck route and asynchandler wrapper

--- src/app.js
+++ src/app.js
@@ -23,6 +23,12 @@ app.use(cors({
 
 
 
+// import routes
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter)
+
 
 
 app.get("/", (req, res) => {
