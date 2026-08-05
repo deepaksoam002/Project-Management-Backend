4f057d39751020149aa629cba800e6dd4c254308
deepaksoam002 <deepaksoam319@gmail.com>
2026-08-05 07:37:03 +0530

    Standard ApiResponse and ApiError

--- src/app.js
+++ src/app.js
@@ -5,6 +5,24 @@ import cors from "cors";
 
 const app = express();
 
+// basic configurations
+
+app.use(express.json({ limit:"16kb" }));
+app.use(express.urlencoded({ extended: true, limit:"16kb" }));
+app.use(express.static("public"));
+
+
+// CORS configurations 
+
+app.use(cors({
+    origin:process.env.ORIGIN?.split(",") || "http://localhost:5173",
+    credentials:true,
+    methods:["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders:["Content-Type", "Authorization"]
+}));
+
+
+
 
 
 app.get("/", (req, res) => {
